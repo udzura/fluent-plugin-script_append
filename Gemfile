62f355b3f7f065b7a9774529b5b94a0bde862ef5
Uchio KONDO <udzura@pepabo.com>
2014-12-26 12:40:52 +0900

    Initial bundle gem'd

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in fluent-plugin-script_append.gemspec
+gemspec
---
+++ lib/fluent/plugin/script_append.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/script_append/version"
+
+module Fluent
+  module Plugin
+    module ScriptAppend
+      # Your code goes here...
+    end
+  end
+end
